aeaa973b281ce3e2d2bdf3793011f39a4d64f129
W1zzK1 <vladgorr1262@gmail.com>
2022-12-26 14:18:58 +0300

    main

---
+++ Containers/Containers.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> numbers = { 1, 2, 3, 4, 5, 5};
+
+    int maximum = numbers[0];
+    int n = numbers.size();
+    int k = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (numbers[i] > maximum)
+        {
+            maximum = numbers[i];
+            k = 1;
+        }
+        else if (numbers[i] == maximum)
+            ++k;
+    }
+    int max = *max_element(numbers.begin(), numbers.end());
+
+    cout << max << endl;
+    cout << k;
+    return 0;
+}
+
